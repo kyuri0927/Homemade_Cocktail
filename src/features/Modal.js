10cfd90aa599bc642f3f018c189c7d817154e0d8
조기범 <akfmzh1207@naver.com>
2022-10-08 22:44:45 +0900

    모달창 구현중

---
+++ src/features/Modal.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//  참고 https://joylee-developer.tistory.com/184
+function Modal(props) {
+  function closeModal() {
+    props.closeModal();
+  }
+
+  return (
+    <div className="Modal" onClick={closeModal}>
+      <div className="modalBody" onClick={(e) => e.stopPropagation()}>
+        <button id="modalCloseBtn" onClick={closeModal}>
+          ✖
+        </button>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
