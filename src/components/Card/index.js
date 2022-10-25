d9a15e1ff8985fa358be4b8c71986d2a9094a792
조기범 <akfmzh1207@naver.com>
2022-10-25 19:53:01 +0900

    Card 컴포넌트 구현

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import * as Styled from "./styled";
+import Data from "../Data";
 
 function Card() {
-  return <></>;
+  //   const card = Data.map((a, i) => {
+  //     <div>
+  //       <h4>{a}</h4>
+  //     </div>;
+  //   });
+
+  return (
+    <>
+      {[1, 2, 3, 4, 5, 6].map(function () {
+        return (
+          <div>
+            <Styled.Box>
+              <p>술사진</p>
+              <p>술이름</p>
+              <p>평점</p>
+            </Styled.Box>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Card;
---
+++ src/components/Card/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  //칵테일 나열 박스
+  display: relative;
+  width: 444px;
+  height: 160px;
+  margin: auto;
+  margin-top: 30px;
+  background-color: #e9ecef;
+  border: 1px solid black;
+  border-radius: 10px;
+`;
